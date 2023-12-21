c2d6bc77375d1c9ae81659376cd55f9ff1451722
adinareiter <144619417+adinareiter@users.noreply.github.com>
2023-12-21 16:00:37 -0500

    Add sessions create action (login)

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,7 @@ Rails.application.routes.draw do
 
   # users
   post "/users" => "users#create"
+
+  # sessions
+  post "/sessions" => "sessions#create"
 end
---
+++ test/controllers/sessions_controller_test.rb
@@ -0,0 +1,12 @@
+require "test_helper"
+
+class SessionsControllerTest < ActionDispatch::IntegrationTest
+  test "create" do
+    post "/users.json", params: { name: "Test", email: "test@test.com", password: "password", password_confirmation: "password" }
+    post "/sessions.json", params: { email: "test@test.com", password: "password" }
+    assert_response 201
+
+    data = JSON.parse(response.body)
+    assert_equal ["jwt", "email", "user_id"], data.keys
+  end
+end
